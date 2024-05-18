721272f95f29a2c7074b76196e01cf80e7e50f1e
Damian Wróblewski <dwroblewski89@gmail.com>
2024-05-18 11:52:42 +0200

    feat(completion-controller): fixed response format + improved code readability and integration

--- app/controllers/completion.controller.js
+++ app/controllers/completion.controller.js
@@ -2,34 +2,29 @@ const db = require('../config/db.connection');
 const openAIconfig = require('../config/openAI.config');
 
 async function getCompletion(req, res) {
-    const body = req.body;
-    const session = body?.user?.session;
-    const openAIrequestBody = body?.openAIrequestBody;
-
-    // Check if the session is valid
-    if (!session) {
-        return res.status(401).send('Unauthorized');
-    }
-
-    // Check if the request body is valid
-    if (!body?.openAIrequestBody || typeof body !== 'object') {
-        return res.status(400).send('Invalid request body');
-    }
-
-    // Check if the user is logged in
     try {
+        const { body } = req;
+        const session = body?.user?.session;
+        const openAIrequestBody = body?.openAIrequestBody;
+
+        // Check if the session is valid
+        if (!session) {
+            return res.status(401).send('Unauthorized');
+        }
+
+        // Check if the request body is valid
+        if (!openAIrequestBody || typeof body !== 'object') {
+            return res.status(400).send('Invalid request body');
+        }
+
+        // Check if the user is logged in
         const user = await db.one('SELECT * FROM "User" WHERE "userAuthToken" = $1', [session]);
-		if (!user) {
-			return res.status(401).send('Unauthorized');
-		}
-    } catch (e) {
-        console.log(e);
-        return res.status(500).send('Server error');
-    }
+        if (!user) {
+            return res.status(401).send('Unauthorized');
+        }
 
-    try {
         // Get the completion from OpenAI
-        const response = await fetch(openAIconfig.openAIapiUrl, {
+        const openAIresponse = await fetch(openAIconfig.openAIapiUrl, {
             method: 'POST',
             headers: new Headers({
                 'Content-Type': 'application/json',
@@ -38,17 +33,22 @@ async function getCompletion(req, res) {
             body: openAIrequestBody,
         });
 
-        console.log({response});
         // Check if the response is ok
-        if (response.status !== 200) {
+        if (openAIresponse.status !== 200) {
             return res.status(500).send('External API error');
         }
 
-        const responseJson = await response.json();
-        return res.send(responseJson);
+        const responseJson = await openAIresponse.json();
+        const generatedPlan = responseJson?.choices[0]?.message?.content;
+
+        if (!generatedPlan || typeof generatedPlan !== 'string' || generatedPlan.length === 0) {
+            return res.status(500).send('External API error - no response text');
+        }
+
+        return res.send(JSON.stringify(generatedPlan));
     } catch (e) {
         console.log(e);
-        res.status(500).send(e.message);
+        res.status(500).send('Server error');
     }
 }
 
