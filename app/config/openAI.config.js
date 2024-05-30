35e65b51ccec545556105a33c7d4111c776d7326
Damian Wróblewski <dwroblewski89@gmail.com>
2024-05-30 20:02:43 +0200

    feat(configuration): changed model to gpt-4o

--- app/config/openAI.config.js
+++ app/config/openAI.config.js
@@ -1,6 +1,7 @@
 const openAIconfig = {
     openAIapiUrl: 'https://api.openai.com/v1/chat/completions',
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4o',
+    // model: 'gpt-3.5-turbo',
     openAIcompletionSeed: 1,
     openAIcompletionTemperature: 0.2,
 };
